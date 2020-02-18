c6c78ae90ab31af75158978d0b48f468262b1a33
oracle <oracle@localhost>
2020-02-18 10:38:35 +0100

    MongoDB

---
+++ src/mongodb/MongoDB.java
@@ -0,0 +1,87 @@
+
+package mongodb;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoDB {
+public void insertarDocumento(){
+       
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+        
+        MongoDatabase database = mongoClient.getDatabase("tenda");
+
+        
+        MongoCollection<Document> collection = database.getCollection("pedidos");
+
+        Document document = new Document();
+
+        document.put("_id", "p4");
+        document.put("codcli", "c1");
+        document.put("codpro", "pro3");
+        document.put("cantidade", 5);
+        document.put("data", "02/02/2019");
+
+        collection.insertOne(document);
+
+}
+public void actualizarDocumento(){
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("tenda");
+    MongoCollection<Document> collection = database.getCollection("pedidos");
+        BasicDBObject doc = new BasicDBObject();
+        doc.put("_id", "p3");
+        BasicDBObject newDoc = new BasicDBObject();
+        newDoc.put("codpro", "pro5");
+
+        BasicDBObject updateObject = new BasicDBObject();
+        updateObject.put("$set", newDoc);
+
+        collection.updateOne(doc, updateObject);
+
+}
+public void incDocumento(){
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("tenda");
+    MongoCollection<Document> collection = database.getCollection("pedidos");
+    BasicDBObject updateObject = new BasicDBObject();
+    BasicDBObject doc = new BasicDBObject();
+    doc.put("_id", "p2");
+
+updateObject.put("$inc", doc);
+    
+}
+
+public void borrarDocument(){
+      MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("tenda");
+    MongoCollection<Document> collection = database.getCollection("pedidos");
+    BasicDBObject searchQuery = new BasicDBObject();
+    BasicDBObject doc = new BasicDBObject();
+searchQuery.put("_id","p2");
+ 
+doc.remove(searchQuery);
+}
+
+
+          
+
+    public static void main(String[] args) {
+      MongoDB mongo = new MongoDB();
+//      mongo.insertarDocumento();
+//      mongo.actualizarDocumento();
+      
+      //METODOS DE INC Y BORRAR NO HACEN NADA, REVISAR POR QUE
+//      mongo.incDocumento();
+       mongo.borrarDocument();
+      
+        
+       
+   
+    
+}
+}
